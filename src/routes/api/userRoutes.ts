d7c76b8b82e48d419a8924255c0aaf2e6be6eaad
Arjun Patel <arjunpatel9217@gmail.com>
2024-12-02 18:51:55 -0600

    created the api routes

---
+++ src/routes/api/userRoutes.ts
@@ -0,0 +1,29 @@
+import { Router } from 'express';
+const router = Router();
+import {
+  getAllUsers,
+  getUserById,
+  createUser,
+  updateUser,
+  deleteUser,
+  addFriend,
+  deleteFriend,
+} from '../../controllers/userController.js';
+
+// /api/users
+router.route('/').get(getAllUsers).post(createUser);
+
+// /api/users/:userId
+router
+  .route('/:userId')
+  .get(getUserById)
+  .put(updateUser)
+  .delete(deleteUser);
+
+ // /api/users/:userId/friends/:friendId
+ router
+  .route('/:userId/friends/:friendId')
+  .post(addFriend)
+  .delete(deleteFriend);
+
+export { router as userRouter };
